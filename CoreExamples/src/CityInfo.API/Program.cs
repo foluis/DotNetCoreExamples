d8a20f12a85d428b59741617d71e1bd48fcd8e86
Luis Fernando Forero G. <foluis@hotmail.com>
2016-11-12 18:55:28 -0600

    Initial Templates

---
+++ CoreExamples/src/CityInfo.API/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace CityInfo.API
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            //var host = new WebHostBuilder();
+        }
+    }
+}
