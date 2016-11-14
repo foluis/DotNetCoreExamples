c65d5028411e1d787df46122d9d721847cdd13bb
Luis Fernando Forero G. <foluis@hotmail.com>
2016-11-13 21:19:14 -0600

    Finalizó modulo 5  Working with Services and Dependency Injection

--- CoreExamples/src/CityInfo.API/Controllers/PointOfInterestController.cs
+++ CoreExamples/src/CityInfo.API/Controllers/PointOfInterestController.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CityInfo.API.Models;
+using CityInfo.API.Services;
+using Microsoft.AspNetCore.JsonPatch;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,19 +13,40 @@ namespace CityInfo.API.Controllers
     [Route("api/Cities")]
     public class PointOfInterestController : Controller
     {
+        private ILogger<PointOfInterestController> _logger;
+        private IMailService _mailService;
+
+        public PointOfInterestController(ILogger<PointOfInterestController> logger, 
+            IMailService localMailService)
+        {
+            _logger = logger;
+            _mailService = localMailService;
+        }
+
         [HttpGet("{CityId}/pointsOfInterest")]
         public IActionResult GetPointOfInterestByCity(int cityId)
         {
-            var city = CitiesDataStore.Current.Cities.FirstOrDefault(x => x.Id == cityId);
-            if (city == null)
+            try
             {
-                return NotFound();
+                //throw new Exception("Example exeption"); //Ejemplo login
+
+                var city = CitiesDataStore.Current.Cities.FirstOrDefault(x => x.Id == cityId);
+                if (city == null)
+                {
+                    _logger.LogInformation($"City with the id {cityId} wasn't found accessing point of interest.");
+                    return NotFound();
+                }
+                return Ok(city.PointsOfInterestDto);
             }
-            return Ok(city.PointsOfInterestDto);
+            catch (Exception ex)
+            {
+                _logger.LogInformation($"Exception while getting points of interest for city with id {cityId}",ex);
+                return StatusCode(500, "A problem happend while handling your request");
+            }
         }
 
-        [HttpGet("{CityId}/pointOfInterest/{id}")]
-        public IActionResult GetPointOfInterest(int cityId,int id)
+        [HttpGet("{CityId}/pointOfInterest/{id}", Name = "GetPointOfInterest")]
+        public IActionResult GetPointOfInterest(int cityId, int id)
         {
             var city = CitiesDataStore.Current.Cities.FirstOrDefault(x => x.Id == cityId);
             if (city == null)
@@ -36,6 +61,149 @@ namespace CityInfo.API.Controllers
             }
 
             return Ok(pointsOfInterestDto);
+        }
+
+        [HttpPost("{CityId}/pointOfInterest")]
+        public IActionResult CreatePointOfInterest(int cityId, [FromBody] PointOfInterestForCreationDto pointOfInterest)
+        {
+            //FluentValidation Validation Library  for .net
+
+            if (pointOfInterest == null)
+            {
+                return BadRequest();
+            }
+
+            if (pointOfInterest.Name == pointOfInterest.Description)
+            {
+                ModelState.AddModelError("Description", "The provided description shoud be diferent from the name");
+            }
+
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var city = CitiesDataStore.Current.Cities.FirstOrDefault(x => x.Id == cityId);
+            if (city == null)
+            {
+                return NotFound();
+            }
+
+            var maxPointOfInterest = CitiesDataStore.Current.Cities
+                .SelectMany(c => c.PointsOfInterestDto)
+                .Max(p => p.Id);
+
+            var newPointOfInterest = new PointOfInterestDto()
+            {
+                Id = ++maxPointOfInterest,
+                Name = pointOfInterest.Name,
+                Description = pointOfInterest.Description
+            };
+
+            city.PointsOfInterestDto.Add(newPointOfInterest);
+
+            return CreatedAtRoute("GetPointOfInterest", new
+            { cityId = city.Id, id = newPointOfInterest.Id }, newPointOfInterest);
+        }
+
+        [HttpPut("{CityId}/pointOfInterest/{id}")]
+        public IActionResult UpdatePointOfInterest(int cityId, int id,
+            [FromBody] PointOfInterestForUpdateDto pointOfInterest)
+        {
+            if (pointOfInterest == null)
+            {
+                return BadRequest();
+            }
+
+            if (pointOfInterest.Name == pointOfInterest.Description)
+            {
+                ModelState.AddModelError("Description", "The provided description shoud be diferent from the name");
+            }
+
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var city = CitiesDataStore.Current.Cities.FirstOrDefault(x => x.Id == cityId);
+            if (city == null)
+            {
+                return NotFound();
+            }
+
+            var pointsOfInterestDto = city.PointsOfInterestDto.FirstOrDefault(x => x.Id == id);
+            if (city == null)
+            {
+                return NotFound();
+            }
+
+            pointsOfInterestDto.Name = pointOfInterest.Name;
+            pointsOfInterestDto.Description = pointOfInterest.Description;
+
+            return NoContent();
+        }
+
+        [HttpPatch("{CityId}/pointOfInterest/{id}")]
+        public IActionResult PartiallyUpdatePointOfInterest(int cityId, int id,
+            [FromBody] JsonPatchDocument<PointOfInterestForUpdateDto> patchDoc)
+        {
+            if (patchDoc == null)
+            {
+                return BadRequest();
+            }
+
+            var city = CitiesDataStore.Current.Cities.FirstOrDefault(x => x.Id == cityId);
+            if (city == null)
+            {
+                return NotFound();
+            }
+
+            var currentPointsOfInterestDto = city.PointsOfInterestDto.FirstOrDefault(x => x.Id == id);
+            if (city == null)
+            {
+                return NotFound();
+            }
+
+            var pointOfInterestToPatch = new PointOfInterestForUpdateDto()
+            {
+                Name = currentPointsOfInterestDto.Name,
+                Description = currentPointsOfInterestDto.Description
+            };
+
+            patchDoc.ApplyTo(pointOfInterestToPatch, ModelState);
+
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            currentPointsOfInterestDto.Name = pointOfInterestToPatch.Name;
+            currentPointsOfInterestDto.Description = pointOfInterestToPatch.Description;
+
+            return NoContent();
+        }
+
+
+        [HttpDelete("{CityId}/pointOfInterest/{id}")]
+        public IActionResult DeletePointOfInterest(int cityId, int id)
+        {
+            var city = CitiesDataStore.Current.Cities.FirstOrDefault(x => x.Id == cityId);
+            if (city == null)
+            {
+                return NotFound();
+            }
+
+            var currentPointsOfInterestDto = city.PointsOfInterestDto.FirstOrDefault(x => x.Id == id);
+            if (city == null)
+            {
+                return NotFound();
+            }
+
+            city.PointsOfInterestDto.Remove(currentPointsOfInterestDto);
+
+            _mailService.Send("Point of interest deleted", $"Point of interest {currentPointsOfInterestDto.Name} with id {currentPointsOfInterestDto.Id} deleted");
+
+            return NoContent();
         }
     }
 }
--- CoreExamples/src/CityInfo.API/Startup.cs
+++ CoreExamples/src/CityInfo.API/Startup.cs
@@ -9,11 +9,26 @@ using Microsoft.AspNetCore.Mvc.Formatters;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json.Serialization;
+using NLog.Extensions.Logging;
+using CityInfo.API.Services;
+using Microsoft.Extensions.Configuration;
 
 namespace CityInfo.API
 {
     public class Startup
     {
+        public static IConfigurationRoot Configuration;
+
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appSettings.json", optional: false, reloadOnChange: true)
+                .AddJsonFile($"appSettings.{env.EnvironmentName}.json", optional: true, reloadOnChange: true);
+
+            Configuration = builder.Build();
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
@@ -31,12 +46,26 @@ namespace CityInfo.API
             //        casterResolver.NamingStrategy = null;
             //    }
             //});
+#if DEBUG
+            services.AddTransient<IMailService, LocalMailService>();
+#else
+            services.AddTransient<IMailService,CloudMailService>();
+#endif
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
             loggerFactory.AddConsole();
+
+            loggerFactory.AddDebug();
+
+            ///Add loging thirparty provider -- www.github.com/aspnet/logging
+            ///Nuget Package - nlog.Extensions.Logging - (Include Prerelease)
+            //Add nlog.config file
+
+            //loggerFactory.AddProvider(new NLog.Extensions.Logging.NLogLoggerProvider());
+            loggerFactory.AddNLog();
 
             if (env.IsDevelopment())
             {
